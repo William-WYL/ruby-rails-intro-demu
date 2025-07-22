ca45badc56674a888ed4db7f03161ed7935dbe1c
Wei(William) Wang <william.wang.uncharted@gmail.com>
2025-07-21 23:12:29 -0500

    Beautify orders, application

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get "orders/index"
-  get "orders/show"
   root "products#index"
 
   resources :users
@@ -11,8 +9,4 @@ Rails.application.routes.draw do
   get "about", to: "pages#about"
 
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # PWA routes (commented out)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 end
